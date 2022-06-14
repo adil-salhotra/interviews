458b46d5e82f9c5c71503d693956a8ba8aefdc09
Adil Salhotra <adilsalhotra@gmaill.com>
2022-06-14 00:12:21 -0700

    .

--- graph_theory/backtracking/test_n_queens.py
+++ graph_theory/backtracking/test_n_queens.py
@@ -2,3 +2,6 @@ from n_queens import n_queens
 
 def test_0():
     assert n_queens(4) == [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+
+def test_1():
+    assert n_queens(1) == [["Q"]]
--- graph_theory/backtracking/test_word_search.py
+++ graph_theory/backtracking/test_word_search.py
@@ -5,3 +5,15 @@ def test_0():
     word = "ABCCED"
 
     assert(word_search(board,word) == True)
+
+def test_1():
+    board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+    word = "SEE"
+    
+    assert(word_search(board,word) == True)
+
+def test_2():
+    board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+    word = "ABCB"
+
+    assert(word_search(board,word) == False)
