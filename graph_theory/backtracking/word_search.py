dd808fb97fb972d667f184611cdf4846c056d16a
Adil Salhotra <adilsalhotra@gmaill.com>
2022-06-13 18:52:57 -0700

    added docstrings

--- graph_theory/backtracking/word_search.py
+++ graph_theory/backtracking/word_search.py
@@ -3,6 +3,10 @@ def word_search(board, word):
     :type board: List[List[str]]
     :type word: str
     :rtype: bool
+
+    Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+    The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are 
+    horizontally or vertically neighboring. The same letter cell may not be used more than once.
     """
     ROWS = len(board)
     COLS = len(board[0])
