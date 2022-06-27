30f88b682f938d0bb9d6ffb774e73caa28c13c83
Adil Salhotra <adilsalhotra@gmaill.com>
2022-06-27 16:10:46 -0700

    .

--- dynamic_programming/test_best_sum.py
+++ dynamic_programming/test_best_sum.py
@@ -9,3 +9,4 @@ def test_1():
     assert sorted(best_sum_memo(7, [5,3,4,7], dict())) == sorted(best_sum(7, [5,3,4,7]))
     assert sorted(best_sum_memo(8, [2,3,5], dict())) == sorted(best_sum(8, [2,3,5]))
     assert sorted(best_sum_memo(8, [1,4,5], dict())) == sorted(best_sum(8, [1,4,5]))
+    assert sorted(best_sum_memo(100, [1,2,3,25], dict())) == [25,25,25,25]
