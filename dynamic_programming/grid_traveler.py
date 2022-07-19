8828cd998f9ce5c319214f2febf82d796f15815a
Adil Salhotra <adilsalhotra@gmaill.com>
2022-07-19 15:54:03 -0700

    tabulation for grid traveler

--- dynamic_programming/grid_traveler.py
+++ dynamic_programming/grid_traveler.py
@@ -24,3 +24,18 @@ def grid_traveler_memo(m, n, memo):
     memo[key] = grid_traveler(m-1, n) + grid_traveler(m, n-1)
     return memo[key]
 
+def grid_traveler_tab(m, n):
+    # Grid with m+1 rows and n+1 columns
+    grid = [[0] * (n+1) for _ in range(m+1)]
+    grid[1][1] = 1
+
+    for row in range(m+1):
+        for col in range(n+1):
+            if col+1 < n+1:
+                grid[row][col+1] += grid[row][col]
+            if row+1 < m+1:
+                grid[row+1][col] += grid[row][col]
+    
+    return grid[m][n]
+
+
--- dynamic_programming/test_grid_traveler.py
+++ dynamic_programming/test_grid_traveler.py
@@ -1,7 +1,13 @@
-from grid_traveler import grid_traveler, grid_traveler_memo
+from grid_traveler import grid_traveler, grid_traveler_memo, grid_traveler_tab
 
 def test_0():
     assert(grid_traveler(10,10) == grid_traveler_memo(10,10, dict()))
     assert(grid_traveler(2,3) == grid_traveler_memo(2,3, dict()))
     assert(grid_traveler(2,1) == grid_traveler_memo(2,1, dict()))
     assert(grid_traveler(1,1) == grid_traveler_memo(1,1, dict()))
+
+def test_1():
+    assert(grid_traveler(10,10) == grid_traveler_tab(10,10))
+    assert(grid_traveler(2,3) == grid_traveler_tab(2,3))
+    assert(grid_traveler(2,1) == grid_traveler_tab(2,1))
+    assert(grid_traveler(1,1) == grid_traveler_tab(1,1))
