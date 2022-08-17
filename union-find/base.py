872aa8044269a131d77e743d1c2c2d82bdce24c1
Adil Salhotra <adilsalhotra@gmaill.com>
2022-08-17 10:21:39 -0700

    .

---
+++ union-find/base.py
@@ -0,0 +1,27 @@
+class UnionFind:
+    def __init__(self) -> None:
+        self.parent = list()
+        self.rank = list()
+
+    def find(self, n):
+        p = self.parent[n]
+        while p != self.parent[p]:
+            self.parent[p] = self.parent[self.parent[p]]
+            p = self.parent[p]
+        return p 
+    
+    def union(self, n1, n2):
+        p1 = self.find(n1)
+        p2 = self.find(n2)
+
+        if p1 == p2:
+            return False
+
+        if self.rank(p1) >= self.rank(p2):
+            self.parent[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+            
